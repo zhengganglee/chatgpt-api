107e8be22afda48e5a5267e8ae2723c8d09a2e0f
lizhenggang <lizhenggang@kingsoft.com>
2023-03-06 20:10:19 +0800

    chatgpt-api RPC远程调用

---
+++ demos/app2.ts
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv-safe'
+import http from 'http'
+import process from 'process'
+import querystring from 'querystring'
+
+import { ChatGPTAPI } from '../src'
+
+dotenv.config()
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST') {
+    let body = ''
+
+    req.on('data', (chunk) => {
+      body += chunk.toString()
+    })
+
+    req.on('end', async () => {
+      const data = querystring.parse(body)
+      if (data.prompt) {
+        const promptValue =
+          typeof data.prompt === 'string' ? data.prompt : data.prompt.join(',')
+        const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY })
+        const response = await api.sendMessage(promptValue)
+
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(response))
+      } else {
+        res.writeHead(400, { 'Content-Type': 'text/plain' })
+        res.end('Bad Request: Invalid prompt parameter')
+      }
+    })
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
+})
+
+const PORT = 3000
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
