5ba9bb3c8752ec6f294a615b5e602a48da952867
lizhenggang <lizhenggang@kingsoft.com>
2023-03-06 16:08:35 +0800

    chatgpt-api RPC远程调用

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,6 +1,5 @@
 /// <reference lib="dom" />
-
-const fetch = globalThis.fetch
-// import fetch from 'isomorphic-fetch'
+// const fetch = globalThis.fetch
+import fetch from 'isomorphic-fetch'
 
 export { fetch }
