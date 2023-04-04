4b43ad6b52376a6576b3516f5c4a14282f115f51
lizhenggang <lizhenggang@kingsoft.com>
2023-04-04 16:29:35 +0800

    chatgpt-api RPC远程调用

--- demos/app2.ts
+++ demos/app2.ts
@@ -25,27 +25,44 @@ const server = http.createServer((req, res) => {
         let opts = msgId ? {parentMessageId: msgId} : {}
         console.log(`############################22 promptValue=${prompt} msgId=${msgId} opts=${JSON.stringify(opts)}`)
 
-        let res2
-        if (msgId) {
-          // const prompt2 = 'Can you make it cuter and shorter?'
-          res2 = await oraPromise(
-              api.sendMessage(prompt, {
-                parentMessageId: msgId
-              }),
-              {
-                text: prompt
-              }
-          )
-          console.log('=====res2\n' + res2.text + '\n')
-        } else {
-          // const prompt = 'Write a poem about cats.'
-          res2 = await oraPromise(api.sendMessage(prompt), {
-            text: prompt
-          })
-          console.log('=====res1\n' + res2.text + '\n')
-        }
+        // let res2
+        // if (msgId) {
+        //   // const prompt2 = 'Can you make it cuter and shorter?'
+        //   res2 = await oraPromise(
+        //       api.sendMessage(prompt, {
+        //         parentMessageId: msgId
+        //       }),
+        //       {
+        //         text: prompt
+        //       }
+        //   )
+        //   console.log('=====res2\n' + res2.text + '\n')
+        // } else {
+        //   // const prompt = 'Write a poem about cats.'
+        //   res2 = await oraPromise(api.sendMessage(prompt), {
+        //     text: prompt
+        //   })
+        //   console.log('=====res1\n' + res2.text + '\n')
+        // }
 
+        const prompt1 = 'Write a poem about cats.'
+        let res2 = await oraPromise(api.sendMessage(prompt1), {
+          text: prompt1
+        })
+        console.log('====1\n' + res2.text + '\n')
+        console.log('====2\n' + JSON.stringify(res2) + '\n')
 
+        const prompt2 = 'Can you make it cuter and shorter?'
+        res2 = await oraPromise(
+            api.sendMessage(prompt2, {
+              parentMessageId: res2.id
+            }),
+            {
+              text: prompt2
+            }
+        )
+        console.log('====3\n' + res2.text + '\n')
+        console.log('====4\n' + JSON.stringify(res2) + '\n')
 
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify(res2))
