d9e34bb9a8e17a8fa508be75c3e9605d0ed14dbc
lizhenggang <lizhenggang@kingsoft.com>
2023-04-04 16:49:34 +0800

    chatgpt-api RPC远程调用

--- demos/app2.ts
+++ demos/app2.ts
@@ -3,10 +3,10 @@ import http from 'http'
 import process from 'process'
 import querystring from 'querystring'
 
-import { oraPromise } from 'ora'
 import { ChatGPTAPI } from '../src'
 
 dotenv.config()
+const api = new ChatGPTAPI({apiKey: process.env.OPENAI_API_KEY})
 
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
@@ -19,45 +19,35 @@ const server = http.createServer((req, res) => {
     req.on('end', async () => {
       const data = querystring.parse(body)
       if (data.prompt) {
-        const api = new ChatGPTAPI({apiKey: process.env.OPENAI_API_KEY})
         const prompt = typeof data.prompt === 'string' ? data.prompt : null
         const msgId = typeof data.msgId === 'string' ? data.msgId : null
         let opts = msgId ? {parentMessageId: msgId} : {}
         console.log(`############################22 promptValue=${prompt} msgId=${msgId} opts=${JSON.stringify(opts)}`)
 
-        // let res2
-        // if (msgId) {
-        //   // const prompt2 = 'Can you make it cuter and shorter?'
-        //   res2 = await oraPromise(
-        //       api.sendMessage(prompt, {
-        //         parentMessageId: msgId
-        //       }),
-        //       {
-        //         text: prompt
-        //       }
-        //   )
-        //   console.log('=====res2\n' + res2.text + '\n')
-        // } else {
-        //   // const prompt = 'Write a poem about cats.'
-        //   res2 = await oraPromise(api.sendMessage(prompt), {
-        //     text: prompt
-        //   })
-        //   console.log('=====res1\n' + res2.text + '\n')
-        // }
+        let res2
+        if (msgId) {
+          res2 = await api.sendMessage(prompt, {
+            parentMessageId: msgId
+          })
+          console.log('=====111\n' + res2.text + '\n')
+        } else {
+          res2 = await api.sendMessage(prompt)
+          console.log('=====aaa\n' + res2.text + '\n')
+        }
 
         // const prompt1 = 'Write a poem about cats.'
         // let res2 = await api.sendMessage(prompt1)
         // console.log('====1\n' + res2.text + '\n')
         // console.log('====2\n' + JSON.stringify(res2) + '\n')
         // console.log('====3\n' + res2.id + '\n')
-
-        const prompt2 = 'Can you make it cuter and shorter?'
-        let res2 = await api.sendMessage(prompt2, {
-          parentMessageId: 'chatcmpl-71WWnLq0IuHbv11jxZeRaaTH7UaN4'
-        })
-        console.log('====a\n' + res2.text + '\n')
-        console.log('====b\n' + JSON.stringify(res2) + '\n')
-        console.log('====c\n' + res2.id + '\n')
+        //
+        // const prompt2 = 'Can you make it cuter and shorter?'
+        // res2 = await api.sendMessage(prompt2, {
+        //   parentMessageId: res2.id
+        // })
+        // console.log('====a\n' + res2.text + '\n')
+        // console.log('====b\n' + JSON.stringify(res2) + '\n')
+        // console.log('====c\n' + res2.id + '\n')
 
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify(res2))
