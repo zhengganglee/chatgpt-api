113a02911e9e7818149ec568a25a12e3bcfa4919
lizhenggang <lizhenggang@kingsoft.com>
2023-04-04 19:55:31 +0800

    chatgpt-api RPC远程调用

--- demos/app2.ts
+++ demos/app2.ts
@@ -22,35 +22,11 @@ const server = http.createServer((req, res) => {
         const prompt = typeof data.prompt === 'string' ? data.prompt : null
         const msgId = typeof data.msgId === 'string' ? data.msgId : null
         let opts = msgId ? {parentMessageId: msgId} : {}
-        console.log(`############################22 promptValue=${prompt} msgId=${msgId} opts=${JSON.stringify(opts)}`)
+        console.log(`收到请求 prompt=${prompt} msgId=${msgId} opts=${JSON.stringify(opts)}`)
 
-        let res2
-        if (msgId) {
-          res2 = await api.sendMessage(prompt, {
-            parentMessageId: msgId
-          })
-          console.log('=====111\n' + res2.text + '\n')
-        } else {
-          res2 = await api.sendMessage(prompt)
-          console.log('=====aaa\n' + res2.text + '\n')
-        }
-
-        // const prompt1 = 'Write a poem about cats.'
-        // let res2 = await api.sendMessage(prompt1)
-        // console.log('====1\n' + res2.text + '\n')
-        // console.log('====2\n' + JSON.stringify(res2) + '\n')
-        // console.log('====3\n' + res2.id + '\n')
-        //
-        // const prompt2 = 'Can you make it cuter and shorter?'
-        // res2 = await api.sendMessage(prompt2, {
-        //   parentMessageId: res2.id
-        // })
-        // console.log('====a\n' + res2.text + '\n')
-        // console.log('====b\n' + JSON.stringify(res2) + '\n')
-        // console.log('====c\n' + res2.id + '\n')
-
+        let result = await api.sendMessage(prompt, opts)
         res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(res2))
+        res.end(JSON.stringify(result))
       } else {
         res.writeHead(400, { 'Content-Type': 'text/plain' })
         res.end('Bad Request: Invalid prompt parameter')
