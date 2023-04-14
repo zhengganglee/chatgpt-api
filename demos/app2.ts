39d8aa802227bbf26ae0b5ba0bb36e7e7e81f702
lizhenggang <lizhenggang@kingsoft.com>
2023-04-14 11:11:50 +0800

    chatgpt-api RPC远程调用

--- demos/app2.ts
+++ demos/app2.ts
@@ -22,14 +22,14 @@ const server = http.createServer((req, res) => {
         const prompt = typeof data.prompt === 'string' ? data.prompt : null
         const msgId = typeof data.msgId === 'string' ? data.msgId : null
         let opts = msgId ? { parentMessageId: msgId } : {}
-        //console.log(`收到请求 prompt=${prompt} msgId=${msgId} opts=${JSON.stringify(opts)}`)
+        console.log(`收到请求 prompt=${prompt} msgId=${msgId} opts=${JSON.stringify(opts)}`)
 
         let result = await api.sendMessage(prompt, opts)
-        //console.log(`响应结果200 prompt=${prompt} result=${JSON.stringify(result)}`)
+        console.log(`响应结果200 prompt=${prompt} result=${JSON.stringify(result)}`)
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(JSON.stringify(result))
       } else {
-        //console.log(`响应结果400 prompt=${prompt}`)
+        console.log(`响应结果400 prompt=${data}`)
         res.writeHead(400, { 'Content-Type': 'text/plain' })
         res.end('Bad Request: Invalid prompt parameter')
       }
